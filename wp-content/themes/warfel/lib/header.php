0993acc6ab21450660a626ffe39657d70aa4039e
Tim Dalbey <dalbey@towermarketing.net>
2016-06-09 11:29:50 -0400

    Spit out primary navigation

--- wp-content/themes/warfel/lib/header.php
+++ wp-content/themes/warfel/lib/header.php
@@ -1,15 +1,3 @@
 <?php
-//Remove Header Information
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
 remove_action( 'genesis_header', 'genesis_do_header' );
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-
-//Header
-add_action('genesis_header', 'dt_header');
-function dt_header()
-{
-    ?>
-    <!-- HEADER CONTENT -->
-    <?php
-}
-//Header
